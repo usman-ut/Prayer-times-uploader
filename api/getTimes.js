e3c5679d85e0cf2678a0a3766b128fcbd1cbaae5
usman-ut <usmantabassum.ut@gmail.com>
2026-06-07 16:20:43 +0100

    Update getTimes.js

--- api/getTimes.js
+++ api/getTimes.js
@@ -1,2 +1,10 @@
 export default async function handler(req, res) {
-  res.set
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/usman-ut/Prayer-times/main/timetable.json');
+    const data = await response.json();
+    return res.status(200).json(data);
+  } catch(e) {
+    return res.status(500).json({error: e.message});
+  }
+}
